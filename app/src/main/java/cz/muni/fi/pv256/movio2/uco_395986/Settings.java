91627fa2922270bd493ab81be6da1cbc5889b297
Sqeee <Sqeee@users.noreply.github.com>
2017-10-05 21:14:50 +0200

    Initial commit

---
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_395986/Settings.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pv256.movio2.uco_395986;
+
+/**
+ * Created by Sqee on 5.10.2017.
+ */
+public class Settings {
+    public static final String API_KEY = "dcde155c3b8fa240b20617d53be9cad6";
+}
